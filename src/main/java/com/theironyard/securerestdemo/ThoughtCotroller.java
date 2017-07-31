e3f38793035c74f67b3619e05c256f03d1af5ec5
Prakash Subedi <psubedi2026@Prakashs-MBP.lan>
2017-07-31 11:14:58 -0400

    thoughts correction

--- src/main/java/com/theironyard/securerestdemo/ThoughtCotroller.java
+++ src/main/java/com/theironyard/securerestdemo/ThoughtCotroller.java
@@ -17,8 +17,8 @@ public class ThoughtCotroller {
 		
 		thoughts.put("health1", "Go to gym");
 		thoughts.put("health2", "Meditate on scenic views");
-		thoughts.put("wealth1", "Find a job that pays more than bills.");
-		thoughts.put("wealth2", "Stop being robbed by banks.");
+		thoughts.put("wealth1", "Find a job that pays more than bills");
+		thoughts.put("wealth2", "Stop being robbed by banks");
 		thoughts.put("wisdom1", "Envision success");
 		thoughts.put("wisdom2", "Take action");
 		thoughts.put("wisdom3", "Avoid distractions");
